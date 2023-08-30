803b07835c6f6ec340c2865c4ad081e1a9846617
dania <deduany@gmail.com>
2023-08-30 15:49:26 +0200

    Added Navigation and Search test cases:
    Added two e2e test cases for Navigation
     - navigates from general collection to articles
     - navigates to a wrong url
    
    Added two e2e test cases for Search
    
     - tests search using the dropdown menu
     -tests search using the full lists of results

---
+++ tests/Help Center/help_enums.ts
@@ -0,0 +1,6 @@
+export enum pageNotFoundText {
+    pageDoesNotExist = "Uh oh. That page doesn’t exist.",
+    pageIsNotHere = "That page isn’t here anymore",
+    trySearching = "Try searching for your answer",
+    sendUsMessage = "Or just send us a message",
+}
---
+++ tests/Help Center/navigation.spec.ts
@@ -0,0 +1,42 @@
+import {test, expect} from '@playwright/test';
+import {pageNotFoundText} from './help_enums';
+
+test.beforeEach(async ({page}) => {
+    await page.goto('/');
+});
+
+test.describe('Navigation', () => {
+    // test how to get from collections to article
+    test('Should Navigate from general collections to article', async ({page}) => {
+        await page.locator('#general').click();
+
+        await expect(page).toHaveTitle('General | bunch Help Center');
+        await expect(page.getByTestId('collection-photo')).toBeVisible();
+        const generalHeader = page.locator('#general header:has-text("General")');
+        await expect(generalHeader).toBeVisible();
+        await expect(page.locator('#about-bunch')).toBeVisible();
+
+        await page.getByTestId('article-link').first().click();
+
+        await expect(page).toHaveTitle('Why was bunch founded? | bunch Help Center');
+        await expect(page).toHaveURL(/articles\/75085-why-was-bunch-founded$/);
+        await expect(page.getByTestId('article-section')).toBeVisible();
+
+    });
+    test('user inserts wrong url', async ({page}) => {
+
+        const wrongUrl= 'brunchesss'
+        await page.goto(wrongUrl);
+
+
+        const pageDoesNotExist = page.getByText(pageNotFoundText.pageDoesNotExist);
+        const pageIsNotHere = page.getByText(pageNotFoundText.pageIsNotHere);
+        const trySearching = page.getByText(pageNotFoundText.trySearching);
+        const sendUsMessage = page.getByText(pageNotFoundText.sendUsMessage);
+
+        await expect(pageDoesNotExist).toBeVisible();
+        await expect(pageIsNotHere).toBeVisible();
+        await expect(trySearching).toBeVisible();
+        await expect(sendUsMessage).toBeVisible();
+    });
+});
